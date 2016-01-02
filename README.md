b13c0b432476e96e0178bced46c31512e48417e7
Bolo Chavez <bolo@linux-ev8j.site>
2016-01-02 15:07:48 -0600

    2016-01-02

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # C-learning-project
-This is just a small text game made with c that doesnt use any other libraries except the standart input-output. The point of this isnt to make a good game in the least. Its more of a personal learning experiment. If you are experienced with C and care enough to give me suggestions then go ahead but this project probably isnt going anywhere. 
+This is just a small text game made with C using the ncurses library.  The point of this isnt to make a good game in the least. Its more of a 
+personal learning experiment. If you are experienced with C and care enough to give me suggestions then go ahead but this project probably isnt going anywhere. 
--- thing1.c
+++ thing1.c
@@ -1,8 +1,8 @@
-
+#include "ncurses.h"
 #include "stdio.h"
 #include "stdlib.h"
 
-//charecter inventory
+//enemy
 
 
 
@@ -20,7 +20,7 @@ int damage;
 
 
 
-
+// inventory
 
 
 struct inventory  {
@@ -41,6 +41,11 @@ int bombs;
 
 struct inventory player_inv;
 
+
+
+
+
+struct entity {
 
 
 // charecter stats 
@@ -76,11 +81,6 @@ int coordination;
 
 
 
-int easy = 50;
-int difficult = 100;
-int really_hard = 125;
-int nearly_impossible = 175;
-
 // actual skill bases
 
 int small_electronics_base;
@@ -113,13 +113,23 @@ int C_HP;
 int regen;
 int imunity;
 
+
+//experience 
+
+int level;
+
+int xp;
+
+int xp_to_next;
+
 //position values
 
 int X;
 int Y;
 
+};
 
-
+struct entity player;
 
 
 
@@ -132,7 +142,15 @@ int Y;
 
 main (){
 
-printf(" Warning this program contains content innapropriate for audiences of all ages. Please avoid \n" );
+initscr();
+
+printw(" warning game is very incomplete \n" );
+
+
+getch();
+
+clear();
+
 
 charc();
 }
@@ -146,14 +164,14 @@ charc();
 charc(){
 
 
-printf("welcome to the charecter creation tool \n");
-printf("\n \n \n \n \n \n");
+printw("welcome to the charecter creation tool \n");
+printw("\n \n \n \n \n \n");
 
 
-vitality = 20;
-fitness = 20;
-coordination = 20;
-smarts = 20;
+player.vitality = 20;
+player.fitness = 20;
+player.coordination = 20;
+player.smarts = 20;
 
 
 
@@ -163,49 +181,55 @@ long a;
 int b;
 
 for(i=40;i>0;){
-	printf("\n \n \n \n \n \n \n \n \n \n \n \n \n ");
+	printw("\n \n \n \n \n \n \n \n \n \n \n \n \n ");
 
 	
-	printf("vitality");
-	for(b=vitality/5;b>0;b--){printf("+");}
-	printf("\n");
-	printf("fitness");
-	for(b=fitness/5;b>0;b--){printf("+");}
-	printf("\n");
-	printf("smarts");
-	for(b=smarts/5;b>0;b--){printf("+");}
-	printf("\n");
-	printf("coordination");
-	for(b=coordination/5;b>0;b--){printf("+");}
-	printf("\n");
+	printw("vitality");
+	for(b= player.vitality/5;b>0;b--){printw("+");}
+	printw("\n");
+	printw("fitness");
+	for(b= player.fitness/5;b>0;b--){printw("+");}
+	printw("\n");
+	printw("smarts");
+	for(b= player.smarts/5;b>0;b--){printw("+");}
+	printw("\n");
+	printw("coordination");
+	for(b= player.coordination/5;b>0;b--){printw("+");}
+	printw("\n");
+	
 
 
-printf("choos 1 to increase \n (1)vitality (2)fitness (3)smarts (4)coordination \n (%d left)",i);
+printw("choos 1 to increase \n (1)vitality (2)fitness (3)smarts (4)coordination \n (%d left)",i);
 
+refresh();
 
 
 scanf("%li",&a);
+
+clear();
+
+
 if (a<1 || a>4) continue;
 
 switch(a){
 case 1:
-	vitality++;
+	player.vitality++;
 	i--;
 	break;
 case 2:
-	fitness++;
+	player.fitness++;
 	i--;
 	break;
 case 3:
-	smarts++;
+	player.smarts++;
 	i--;
 	break;
 case 4:
-	coordination++;
+	player.coordination++;
 	i--;
 	break;
 default:
-	printf("unrecognized choice");
+	printw("unrecognized choice");
 	break;
 }
 
@@ -214,7 +238,7 @@ default:
 }
 
 
-printf("done \n ");
+printw("done \n ");
 
 
 
@@ -224,32 +248,36 @@ printf("done \n ");
 
 
 
-small_electronics = small_electronics_base + smarts/2;
-computers= computers_base + smarts/2;
-mechanics = mechanics_base + smarts/2;
-locksmith = locksmith_base + smarts/4 + coordination/4;
-chemistry = chemistry_base + smarts/2;
-fighting  = fighting_base + smarts/4 + fitness/4;
-shooting = shooting_base + smarts/4 + coordination/4;
+player.small_electronics = player.small_electronics_base + player.smarts/2;
+player.computers= player.computers_base + player.smarts/2;
+player.mechanics = player.mechanics_base + player.smarts/2;
+player.locksmith = player.locksmith_base + player.smarts/4 + player.coordination/4;
+player.chemistry = player.chemistry_base +player.smarts/2;
+player.fighting  = player.fighting_base + player.smarts/4 + player.fitness/4;
+player.shooting = player.shooting_base + player.smarts/4 + player.coordination/4;
 
 
-HP = fitness;
-regen = fitness/4;
-imunity = vitality;
+player.HP = player.fitness;
+player.regen = player.fitness/4;
+player.imunity = player.vitality;
  
+clear();
+
+printw("resulting stats:\n");
+
+printw("small electronics: %d \n", player.small_electronics);
+printw("computers: %d \n",player.computers);
+printw("mechanics: %d \n",player.mechanics);
+printw("locksmith: %d \n",player.locksmith);
+printw("chemistry: %d \n", player.chemistry);
+printw("fighting: %d \n ", player.fighting);
+printw("shooting: %d \n", player.shooting);
 
 
-printf("resulting stats:\n");
+refresh();
 
-printf("small electronics: %d \n",small_electronics);
-printf("computers: %d \n",computers);
-printf("mechanics: %d \n",mechanics);
-printf("locksmith: %d \n",locksmith);
-printf("chemistry: %d \n", chemistry);
-printf("fighting: %d \n ", fighting);
-printf("shooting: %d \n", shooting);
-
-
+printw("press any key to continue \n");
+getch();
 
 plot();
 
@@ -265,30 +293,33 @@ plot();
 
 int plot(){
 
-C_HP = HP;
-X=0;
-Y=0;
+player.C_HP = player.HP;
+player.X=0;
+player.Y=0;
 
 int a;
 
-while(C_HP>0){
+while(player.C_HP>0){
 
 a = 0;
 
-printf("Health: %d / %d \n ",C_HP,HP);
+printw("Health: %d / %d \n ",player.C_HP,player.HP);
 
-printf("What to do?: (1)go (2)list inventory (3)scavange (4) quit game \n");
+printw("What to do?: (1)go (2)list inventory (3)scavange (4) quit game \n");
 
+refresh();
 scanf("%d",&a);
+clear();
 
 switch(a){
 case 1:
-	move();
+	shift();
 	break;
 case 2: 
-	printf("bandages:%d \n ", player_inv.bandages);
+	printw("bandages:%d \n ", player_inv.bandages);
 	break;
 case 4:
+	endwin();
 	return 1;
 	
 default:
@@ -307,32 +338,35 @@ default:
 
 //this is the movement function for the main charecter
 
-move(){
+shift(){
 
 int a;
-printf("move where? \n ");
-printf("(1)up (2)down (3)left (4)right \n");
+printw("move where? \n ");
+printw("(1)up (2)down (3)left (4)right \n");
+
+refresh();
 scanf("%d",&a);
+clear();
 
 switch (a){
 case 1:
-	X++;
+	player.X++;
 	break;
 case 2:
-	X--;
+	player.X--;
 	break;
 case 3:
-	Y--;
+	player.Y--;
 	break;
 case 4:
-	Y++;
+	player.Y++;
 	break;
 default:
 	break;
 
 }
 
-printf("position %d,%d \n ",X,Y);
+printw("position %d,%d \n ",player.X,player.Y);
 encounter();
 
 }
@@ -354,35 +388,150 @@ int roll = (rand()%5)+1;
 switch(roll){
 
 case 1:
-	printf("encounter 1 will be combat \n ");
+	printw("encounter 1 will be combat \n ");
+	combat();
 	break;
 case 2:
-	printf("encounter 2 will be an obstacle or trap \n ");
+	printw("encounter 2 will be an obstacle or trap \n ");
 	break;
 
 case 3:
-	printf("encounter 3 will be some sort fo low grade loot \n ");
+	printw("encounter 3 will be some sort fo low grade loot \n ");
 	player_inv.bandages++;
-	printf("you got a bandage! \n ");
+	printw("you got a bandage! \n ");
 	break;
 case 4: 
-	printf("encounter 4 willl be some sort of useful loot like food or bandages \n ");
+	printw("encounter 4 willl be some sort of useful loot like food or bandages \n ");
 	break;
 case 5:
-	printf("encounter 5 will be some larger amount of 4's loot \n ");
+	printw("encounter 5 will be some larger amount of 4's loot \n ");
 	break;
 case 6: 
-	printf("encounter 6 will be a new weapon or high grade loot \n");
+	printw("encounter 6 will be a new weapon or high grade loot \n");
 
 default:
-	printf("something has gone wrong");
+	printw("something has gone wrong");
 	break;
+
+refresh();
+
+
+}
+
+
+}
+
+
+
+
+looting(){
+
+int a = (rand()%6 +1);
+
+switch(a){
+
+case 1:
+	player_inv.bandages++;
+
+case 2: 
+	
+
+default:
+	break;
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 }
 
 
+
+}
+
+
+
+
+
+combat(){
+
+
+int a;
+
+struct enemy enemy1 ;
+
+enemy1.health = 30;
+
+enemy1.damage = 6;
+
+
+
+while (player.C_HP > 0){
+
+printw("What do you wawnt to do? \n ");
+
+printw("(1) attack (2) run (3)item \n ");
+
+refresh();
+
+scanf("%d",&a);
+
+switch(a){
+
+case 1:
+	enemy1.health = enemy1.health - ((player.fighting/2) + (rand() % (player.fighting/2)+1));
+	printw("enemy hp: %d", enemy1.health);
+	break;
+case 2:
+	printw("you attempt to run.... \n " );
+	return;
+
+case 3:
+	 printw("inventory stuff goes here.\n ");
+	break;
+
+default:
+	printw("something went wrong");
+	break;
+
+
+}
+
+if (enemy1.health  <= 0 ) return;
+
+
+
+player.C_HP = player.C_HP - (rand() % enemy1.damage +1 );
+
+printw("player.HP : %d", player.C_HP );
+
+
+
+
+
+
+}
+
+}
+
+
+
+
+levelup(){
+
+
+player.level++;
+
+player.xp_to_next = player.xp_to_next + player.level * player.xp_to_next/4;
 
 
 
